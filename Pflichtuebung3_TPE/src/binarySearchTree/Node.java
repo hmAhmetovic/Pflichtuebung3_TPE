3650bb60415c305cfb63d6909714e35e5de04f6c
1414534 <1414534@141.19.7.33>
2014-11-14 11:02:18 +0100

    Updates:
    Implementierung der Klasse 'Node

--- Pflichtuebung3_TPE/src/binarySearchTree/Node.java
+++ Pflichtuebung3_TPE/src/binarySearchTree/Node.java
@@ -1,5 +1,62 @@
 package binarySearchTree;
 
-public class Node {
+/**
+ * Klasse Node zur Erstellung von Knoten. Ein Knoten hat einen Schlüssel, Wert und 2 Kinderknoten(links,rechts)
+ * @author TPE Gruppe 14
+ *
+ * @param <Key>
+ * @param <T>
+ */
+public class Node<Key, T> {
 
+	// Attribute eines Knotens
+	private Key key;
+	private T value;
+	private Node<Key, T> left;
+	private Node<Key, T> right;
+	
+	// Konstruktor
+	public Node(Key key, T value, Node<Key, T> left, Node<Key, T> right) {
+		
+		this.key = key;
+		this.value = value;
+		this.left = left;
+		this.right = right;
+	}
+	
+	// Getters & Setters Node
+	public Key getKey(){
+		return this.key;
+	}
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	
+	public T getValue(){
+		return this.value;
+	}
+	public void setValue(T value) {
+		this.value = value;
+	}
+
+	
+	public Node<Key, T> getLeft(){
+		return this.left;
+	}
+	public void setLeft(Node<Key, T> left) {
+		this.left = left;
+	}
+
+	
+	public Node<Key, T> getRight(){
+		return this.right;
+	}
+	public void setRight(Node<Key, T> right) {
+		this.right = right;
+	}
+	
+	
+	
+	
 }
