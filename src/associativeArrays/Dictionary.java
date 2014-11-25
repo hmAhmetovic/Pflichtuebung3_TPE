0b3d39602906264760f5aed47349d64cd23a6aec
c.huber <c.huber@141.19.144.65>
2014-11-25 17:05:17 +0100

    Test

---
+++ src/associativeArrays/Dictionary.java
@@ -0,0 +1,14 @@
+package associativeArrays;
+
+public class Dictionary extends GenericAssociativeArray<String,String>{
+
+	public Dictionary(String key, String value) {
+		super(key, value);
+		// TODO Auto-generated constructor stub
+	}
+
+public String[] keys(){
+	
+}
+	
+}
